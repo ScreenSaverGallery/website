144e784b556366bcef4af0d9ba7a42261579406a
Tomáš Javůrek <info@metazoa.org>
2020-02-09 12:27:39 +0100

    imporve

--- src/app/services/wp/wp.service.ts
+++ src/app/services/wp/wp.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
 // services
 import { WpApiLibService } from 'wp-api-lib';
 // rxjs
 import {
   BehaviorSubject,
-  Observable
+  Observable,
+  Subject
 } from 'rxjs';
 
 @Injectable({
@@ -19,12 +21,12 @@ export class WpService {
   private _tags: any[] = [];
   private _media: any[] = [];
 
-  posts: BehaviorSubject<any> = new BehaviorSubject(null);
+  posts: BehaviorSubject<HttpResponse<any>> = new BehaviorSubject(null);
   categories: BehaviorSubject<any> = new BehaviorSubject(null);
-  users: BehaviorSubject<any> = new BehaviorSubject(null);
+  users: BehaviorSubject<HttpResponse<any>> = new BehaviorSubject(null);
   pages: BehaviorSubject<any> = new BehaviorSubject(null);
-  tags: BehaviorSubject<any> = new BehaviorSubject(null);
-  media: BehaviorSubject<any> = new BehaviorSubject(null);
+  tags: BehaviorSubject<HttpResponse<any>> = new BehaviorSubject(null);
+  media: BehaviorSubject<HttpResponse<any>> = new BehaviorSubject(null);
 
   numberOfPosts: number = 0;
   maxResults: number = 100;
@@ -33,33 +35,60 @@ export class WpService {
     private wpApiService: WpApiLibService
   ) { }
 
+  /* POSTS */
+
   getPosts(): void {
-    this.wpApiService.getPosts(`per_page=${this.maxResults}`).subscribe((res: any) => {
-      if (res) {
+    this.wpApiService.getPosts().subscribe((res: HttpResponse<any>) => {
+      if (res && res.body) {
+        // console.log('getPosts', res);
         this.posts.next(res);
-        this._posts = res;
+        this._posts = res.body;
       }
     });
   }
 
-  getPostsOfCategory(id: number): Observable<any> {
-    return this.wpApiService.getPosts(`per_page=${this.maxResults}&categories=${id}&orderby=date`);
+  getPostBySlug(slug: string): Observable<HttpResponse<any>> {
+    return this.wpApiService.getPosts(`slug=${slug}`);
   }
 
-  getCategories(): void {
-    // if (this._categories.length === 0) {
-      this.wpApiService.getCategories(`per_page=${this.maxResults}&orderby=count&order=desc`).subscribe((res: any) => {
-        if (res) {
-          // count posts
-          res.map((category: any) => {
-            this.numberOfPosts += category.count;
-          });
-          // console.log('numberOfPosts', this.numberOfPosts);
-          this._categories = res;
-          this.categories.next(res);
+  getPostsByTag(tag: string): Observable<HttpResponse<any>> {
+    return this.wpApiService.getPosts(`tag=${tag}`);
+  }
+
+  getCategoryPosts(id: number, page: number = 1): Observable<HttpResponse<any>> {
+    return this.wpApiService.getPosts(`categories=${id}&orderby=date&page=${page}`);
+  }
+
+  /* CATEGORIES */
+
+  getAllCategories(): void {
+    this.wpApiService.getCategories('orderby=count&order=desc').subscribe((res: HttpResponse<any>) => {
+      if (res && res.body && res.body.length > 0) {
+        const temp: any[] = this._categories;
+        const total = Number(res.headers.get('X-WP-Total'));
+        const totalPages = Number(res.headers.get('X-WP-TotalPages'));
+        this._categories = temp.concat(res.body);
+        this.categories.next(this._categories);
+        // load every category
+        if (total > this._categories.length) {
+          for (let i = 2; i <= totalPages; i++) {
+            // console.log('getCategories page', i);
+            this.getCategories(i);
+          }
         }
-      });
-    // }
+      }
+    });
+  }
+
+  getCategories(page: number): void {
+    this.wpApiService.getCategories(`orderby=count&order=desc&page=${page}`).subscribe((res: HttpResponse<any>) => {
+      if (res && res.body && res.body.length > 0) {
+        // concat arrays
+        const temp: any[] = this._categories;
+        this._categories = temp.concat(res.body);
+        this.categories.next(this._categories);
+      }
+    });
   }
 
   getCategoryBySlug(slug: string): any {
@@ -75,6 +104,74 @@ export class WpService {
     return null;
   }
 
+  categoryChildren(id: number): number[] {
+    let i = 0;
+    const len = this._categories.length;
+    const result: number[] = [];
+    for (i; i < len; i++) {
+      const cat: any = this._categories[i];
+      if (cat.parent === id) {
+        result.push(cat);
+      }
+    }
+    return result;
+  }
+
+  
+
+  
+
+  /* USERS */
+
+  getUsers(): void {
+    this.wpApiService.getUsers(`orderby=name`).subscribe((res: any) => {
+      if (res && res.body) {
+        // console.log('getUsers', res);
+        this._users = res.body;
+        this.users.next(res);
+      }
+    });
+  }
+
+  getUser(id: number): Observable<any> {
+    return this.wpApiService.getUser(id);
+  }
+
+  /* PAGES */
+
+  getAllPages(): void {
+    // console.log('getAllPages');
+    this.wpApiService.getPages().subscribe((res: HttpResponse<any>) => {
+      if (res && res.body && res.body.length > 0) {
+        const temp: any[] = this._pages;
+        const total = Number(res.headers.get('X-WP-Total'));
+        const totalPages = Number(res.headers.get('X-WP-TotalPages'));
+
+        this._pages = temp.concat(res.body);
+        this.pages.next(this._pages);
+
+        if (total > this._pages.length) {
+          for (let i = 2; i <= totalPages; i++) {
+            // console.log('getCategories page', i);
+            this.getPages(i);
+          }
+        }
+
+      }
+    });
+  }
+
+  getPages(pageNumber: number = 1): void {
+    this.wpApiService.getPages(`page=${pageNumber}`).subscribe((res: HttpResponse<any>) => {
+      if (res && res.body && res.body.length > 0) {
+        // console.log('getPages', res);
+        const temp: any[] = this._pages;
+        this._pages = temp.concat(res.body);
+        this.pages.next(this._pages);
+      }
+    });
+  }
+
   getPageBySlug(slug: string): number {
     if (this._pages.length > 0) {
       let i: number = 0;
@@ -88,44 +185,36 @@ export class WpService {
     return null;
   }
 
-  getPostBySlug(slug: string): Observable<any> {
-    return this.wpApiService.getPosts(`slug=${slug}`);
-  }
-
-  getUsers(): void {
-    this.wpApiService.getUsers(`per_page=${this.maxResults}&orderby=name`).subscribe((res: any) => {
-      if (res) {
-        this._users = res;
-        this.users.next(res);
-      }
-    });
-  }
-
-  getPages(): void {
-    this.wpApiService.getPages().subscribe((res: any) => {
-      if (res) {
-        this._pages = res;
-        this.pages.next(res);
-      }
-    });
-  }
+  /* TAGS */
 
   getTags(): void {
-    this.wpApiService.getTags(`per_page=${this.maxResults}&orderby=name`).subscribe((res: any) => {
-      if (res) {
-        this._tags = res;
+    this.wpApiService.getTags(`orderby=name`).subscribe((res: any) => {
+      if (res && res.body) {
+        // console.log('getTags', res);
+        this._tags = res.body;
         this.tags.next(res);
       }
     });
   }
 
+  getTag(id: number): Observable<any> {
+    return this.wpApiService.getTag(id);
+  }
+
+  /* MEDIAS */
+
   getMedias(): void {
     this.wpApiService.getMedias().subscribe((res: any) => {
-      if (res) {
-        this._media = res;
+      if (res && res.body) {
+        console.log('getMedias', res);
+        this._media = res.body;
         this.media.next(res);
       }
     });
+  }
+
+  getMedia(id: number): Observable<any> {
+    return this.wpApiService.getMedia(id);
   }
   // ...
 }
