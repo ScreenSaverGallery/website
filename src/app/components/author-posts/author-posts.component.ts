ebc267aea3da93fb8cafb9d5339a386fbdc3576a
Tomáš Javůrek <info@metazoa.org>
2020-02-09 12:31:47 +0100

    init

---
+++ src/app/components/author-posts/author-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-author-posts',
+  templateUrl: './author-posts.component.html',
+  styleUrls: ['./author-posts.component.scss']
+})
+export class AuthorPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
