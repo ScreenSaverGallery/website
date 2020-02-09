ab81f26e9aa07a334dd8e0082de4fb5d5b9bf42c
Tomáš Javůrek <info@metazoa.org>
2020-02-09 12:28:11 +0100

    init

---
+++ src/app/services/menu/menu.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+// rxjs
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  private _opened: boolean = false;
+  opened: BehaviorSubject<boolean> = new BehaviorSubject(this._opened);
+
+  constructor() { }
+
+  open(): void {
+    this._opened = true;
+    this.emitOpened();
+  }
+
+  close(): void {
+    this._opened = false;
+    this.emitOpened();
+  }
+
+  toggle(): void {
+    this._opened = !this._opened;
+    this.emitOpened();
+  }
+
+  private emitOpened(): void {
+    this.opened.next(this._opened);
+  }
+}
