fde6b36981baa254eeb8a7319fa29b6903049b1b
Tomáš Javůrek <info@metazoa.org>
2020-02-09 12:29:17 +0100

    change input

--- src/app/components/tag/tag.component.ts
+++ src/app/components/tag/tag.component.ts
@@ -3,6 +3,8 @@ import {
   OnInit,
   Input
 } from '@angular/core';
+// services
+import { WpService } from '../../services/wp/wp.service';
 
 @Component({
   selector: 'app-tag',
@@ -11,12 +13,24 @@ import {
 })
 export class TagComponent implements OnInit {
 
-  @Input() name: string;
-  @Input() link: string;
+  @Input() id: number;
 
-  constructor() { }
+  tag: any;
+
+  constructor(
+    private wpService: WpService
+  ) { }
 
   ngOnInit() {
+    if (this.id) {
+      this.wpService.getTag(this.id).subscribe((res: any) => {
+        // console.log('tag res', res);
+        if (res.status === 200) {
+          this.tag = res.body;
+        }
+      });
+    }
+    
   }
 
 }
