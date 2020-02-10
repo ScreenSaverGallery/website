3ccf24525c11f02e5e4cc290673577270026a1a8
Tomáš Javůrek <info@metazoa.org>
2020-02-10 19:59:27 +0100

    move

---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
