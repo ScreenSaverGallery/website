48d7efc904f9386b74925c5f08ae858354af940a
Tomáš Javůrek <info@metazoa.org>
2020-02-10 10:30:05 +0100

    refactor

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
-import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit, Input } from '@angular/core';
 // services
 import { WpService } from '../../services/wp/wp.service';
 
@@ -11,6 +9,20 @@ import { WpService } from '../../services/wp/wp.service';
 })
 export class PostsComponent implements OnInit {
 
+  @Input() set postsOf(value: string) {
+    this._postsOf = value;
+    // reset
+    this.posts = [];
+    this.postsPage = 1;
+    this.loadPosts();
+  };
+  @Input() set postsFrom(value: string) {
+    this._postsFrom = value;
+  };
+
+  private _postsOf: string;
+  private _postsFrom: string;
+
   category: any;
   children: any[] = [];
   posts: any[] = [];
@@ -20,53 +32,100 @@ export class PostsComponent implements OnInit {
   totalPages: number;
   loadingPosts: boolean = false;
 
+  title: string;
+
   constructor(
-    private route: ActivatedRoute,
     private wpService: WpService
   ) { }
 
   ngOnInit() {
-    // console.log('init posts.component');
-    // TODO TAGS, YEAR ARCHIVES
+    if (this._postsFrom && this._postsOf) {
+      this.loadPosts();
+    }
+  }
+
+  private loadPosts(): void {
+    console.log('postsFrom', this._postsFrom);
+    switch (this._postsFrom) {
+      case 'archive':
+        this.getCategoryPosts(this._postsOf);
+        break;
+      case 'tag':
+        this.getTagPosts(this._postsOf);
+        break;
+      case 'author':
+        this.getAuthorPosts(this._postsOf);
+        break;
+      /* default:
+        this.getCategoryPosts(this._postsOf);
+        break; */
+    }
+  }
+
+  getCategoryPosts(categorySlug: string): void {
     this.wpService.categories.subscribe((res: any) => {
       if (res) {
-        this.route.url.subscribe((url: UrlSegment[]) => {
-          // console.log('url', url);
-          this.postsPage = 1; // reset
-          if (url.length > 0) {
-            const lastUrlPath: number = url.length - 1;
-            this.category = this.wpService.getCategoryBySlug(url[lastUrlPath].path);
-            this.children = this.wpService.categoryChildren(this.category.id);
-            this.posts = []; // reset
-            if (this.children.length === 0) {
-              this.getCategoryPosts();
-            }
-          }
+        this.category = this.wpService.getCategoryBySlug(categorySlug);
+        this.children = this.wpService.categoryChildren(this.category.id);
+        // set title
+        this.title = this.category.name;
+        // console.log('children', this.children);
+        if (this.children.length === 0) {
+          this.loadingPosts = true;
+          this.wpService.getCategoryPosts(this.category.id, this.postsPage).subscribe((res: any) => {
+            this.recievePosts(res);
+          });
+        }
+      }
+    });
+  }
+
+  getAuthorPosts(authorSlug: string): void {
+    this.wpService.getUserBySlug(authorSlug).subscribe((res: any) => {
+      if (res.status === 200) {
+        const author = res.body[0];
+        // console.log('author', author);
+        this.title = (author.name === '&amp;' ? '&' : author.name);
+        this.wpService.getPostsByAuthor(author.id, this.postsPage).subscribe((r: any) => {
+          this.recievePosts(r);
         });
       }
     });
   }
 
-  getCategoryPosts(): void {
-    this.loadingPosts = true;
-    this.wpService.getCategoryPosts(this.category.id, this.postsPage).subscribe((res: HttpResponse<any>) => {
-      if (res && res.body && res.body.length > 0) {
-        this.loadingPosts = false;
-        const temp: any[] = this.posts;
-        this.total = Number(res.headers.get('X-WP-Total'));
-        this.totalPages = Number(res.headers.get('X-WP-TotalPages'));
-        if (this.posts.length < this.total) {
-          this.posts = temp.concat(res.body);
-        }
-        // load more posts
-        if (this.shouldLoadMore()) {
-          this.postsPage++;
-        } 
-      };
+  getTagPosts(tagSlug: string): void {
+    this.wpService.getTagBySlug(tagSlug).subscribe((res: any) => {
+      if (res.status === 200) {
+        // console.log('getTagBySlug', res);
+        const tag = res.body[0];
+        // console.log('tag', tag);
+        this.title = tag.name;
+        this.wpService.getPostsByTag(tag.id, this.postsPage).subscribe((r: any) => {
+          this.recievePosts(r);
+        });
+      }
     });
+    
+  }
+
+  private recievePosts(res: any): void {
+    if (res && res.body && res.body.length > 0) {
+      this.loadingPosts = false;
+      const temp: any[] = this.posts;
+      this.total = Number(res.headers.get('X-WP-Total'));
+      this.totalPages = Number(res.headers.get('X-WP-TotalPages'));
+      if (this.posts.length < this.total) {
+        this.posts = temp.concat(res.body);
+      }
+      // load more posts
+      if (this.shouldLoadMore()) {
+        this.postsPage++;
+      } 
+    }
   }
 
   shouldLoadMore(): boolean {
+    // console.log('shouldLoadMore', this.total > this.posts.length && this.postsPage < this.totalPages + 1);
     if (this.total > this.posts.length && this.postsPage < this.totalPages + 1) {
       return true;
     }
@@ -74,8 +133,9 @@ export class PostsComponent implements OnInit {
   }
 
   loadMorePosts(load: boolean): void {
+    // console.log('loadMorePosts', load);
     if (load && !this.loadingPosts) {
-      this.getCategoryPosts();
+      this.loadPosts();
     }
   }
 
