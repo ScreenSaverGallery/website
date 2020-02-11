09411dd9f4b0761d7b3fca38424503712c3c1477
Tomáš Javůrek <info@metazoa.org>
2020-02-11 11:23:32 +0100

    wrap content of linsk

--- src/app/pipes/demetazoa.pipe.ts
+++ src/app/pipes/demetazoa.pipe.ts
@@ -6,9 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DemetazoaPipe implements PipeTransform {
 
   transform(value: string): string {
+    // console.log('demetazoa value', value);
     const host: any = window.location.hostname;
-    const replace: string = (host === 'localhost' ? '<a href="http://localhost:4200' : '<a href="https://screensaver.gallery');
-    const result = value.replace(/\<a\ href\=\"https\:\/\/screensaver\.metazoa\.org/g, replace);
+    const replaceHost: string = (host === 'localhost' ? '<a href="http://localhost:4200' : '<a href="https://screensaver.gallery');
+    const linkPattern: RegExp = /(\<a[^\>]*>)(.*?)(\<\/a\>)/g;
+    let result = value.replace(/(\<a\ href\=\"https\:\/\/screensaver\.metazoa\.org)|(\<a\ href\=\"http\:\/\/screensaver\.metazoa\.org)/g, replaceHost);
+    result = result.replace(linkPattern, '$1<span class="link-content">$2</span>$3'); // wprap lins with link-container
+
     return result;
   }
 
