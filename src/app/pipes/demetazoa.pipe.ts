8be05b5f2c03a2ff88923be20f9a3d1a1517af9f
Tomáš Javůrek <info@metazoa.org>
2020-02-12 10:02:36 +0100

    correct

--- src/app/pipes/demetazoa.pipe.ts
+++ src/app/pipes/demetazoa.pipe.ts
@@ -8,9 +8,13 @@ export class DemetazoaPipe implements PipeTransform {
   transform(value: string): string {
     // console.log('demetazoa value', value);
     const host: any = window.location.hostname;
-    const replaceHost: string = (host === 'localhost' ? '<a href="http://localhost:4200' : '<a href="https://screensaver.gallery');
+    const replaceHost: string = (host === 'localhost' ? 'http://localhost:4200' : 'https://screensaver.gallery');
     const linkPattern: RegExp = /(\<a[^\>]*>)(.*?)(\<\/a\>)/g;
-    let result = value.replace(/(\<a\ href\=\"https\:\/\/screensaver\.metazoa\.org)|(\<a\ href\=\"http\:\/\/screensaver\.metazoa\.org)/g, replaceHost);
+    const hrefPattern: RegExp = /(href\=\"(http|https)\:\/\/screensaver\.metazoa\.org)/g;
+    console.log(value.match(hrefPattern));
+    // let result = value.replace(/\<a\ href\=\"https\:\/\/screensaver\.metazoa\.org/g, replaceHost);
+    // result = result.replace(/\<a\ href\=\"http\:\/\/screensaver\.metazoa\.org/g, replaceHost);
+    let result = value.replace(hrefPattern, `href="${replaceHost}`);
     result = result.replace(linkPattern, '$1<span class="link-content">$2</span>$3'); // wprap lins with link-container
 
     return result;
