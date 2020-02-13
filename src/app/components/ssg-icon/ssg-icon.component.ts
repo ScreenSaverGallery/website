9cdb6cb564ce35506e37aa7d831d894a89c86c05
Tomáš Javůrek <info@metazoa.org>
2020-02-13 12:12:45 +0100

    add flat version + improve component

--- src/app/components/ssg-icon/ssg-icon.component.ts
+++ src/app/components/ssg-icon/ssg-icon.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Input,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-ssg-icon',
   templateUrl: './ssg-icon.component.html',
   styleUrls: ['./ssg-icon.component.scss']
 })
-export class SsgIconComponent implements OnInit {
+export class SsgIconComponent implements OnInit, AfterViewInit {
 
   @Input() size: number = 100;
   @Input() animated: boolean = false;
+  @Input() flat: boolean = false;
+  @Input() flatColor: string = 'white';
+
+  flatStyle: string;
 
   constructor(
     private elm: ElementRef
@@ -18,6 +28,19 @@ export class SsgIconComponent implements OnInit {
     this.elm.nativeElement.style.width = `${this.size}px`;
     if (this.animated) {
       this.elm.nativeElement.classList.add('animated');
+    }
+    // console.log('flatColor', this.flatColor);
+    this.flatStyle = `.cls-1{fill:${this.flatColor}}`;
+  }
+
+  ngAfterViewInit(): void {
+    if (this.flat) {
+      const svgElms = this.elm.nativeElement.querySelectorAll('.cls-1');
+      // console.log('svgElms', svgElms);
+      for (let i = 0; i < svgElms.length; i++) {
+        const elm = svgElms[i];
+        elm.style.fill = this.flatColor;
+      }
     }
   }
 
