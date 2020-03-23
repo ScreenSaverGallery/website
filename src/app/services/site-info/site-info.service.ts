f85c8ec5f631f676190d4af1e1582ef89c8943c2
Tomáš Javůrek <info@metazoa.org>
2020-03-23 16:37:39 +0100

    create

---
+++ src/app/services/site-info/site-info.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+// services
+import { WpService } from '../wp/wp.service';
+// rxjs
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SiteInfoService {
+
+  siteInfo: BehaviorSubject<any> = new BehaviorSubject(null);
+
+  constructor(
+    private wpServices: WpService
+  ) { }
+
+  getSitInfo(): void {
+    this.wpServices.getSiteInfo().subscribe((res: any) => {
+      if (res && res.status === 200) {
+        this.siteInfo.next(res.body);
+      }
+    });
+  }
+}
