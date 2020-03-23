0b1f129d9282c2e3278277671aa7802f1a407ab4
Tomáš Javůrek <info@metazoa.org>
2020-03-23 16:36:55 +0100

    create social component

---
+++ src/app/components/social/social.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  Output
+} from '@angular/core';
+
+interface Medium {
+  name: string,
+  shortcut: string,
+  urlBase: string
+}
+
+@Component({
+  selector: 'app-social',
+  templateUrl: './social.component.html',
+  styleUrls: ['./social.component.scss']
+})
+export class SocialComponent implements OnInit {
+
+  @Input() medias: Medium[] = []; // no medias default ;)
+  @Input() referenceUrl: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('medias', this.medias);
+    console.log('referenceUrl', this.referenceUrl);
+  }
+
+  getHref(): string {
+    if (this.referenceUrl) return this.referenceUrl;
+    return window.location.href;
+  }
+
+  openHref(href: string): void {
+    window.open(href, '_blank');
+  }
+
+}
