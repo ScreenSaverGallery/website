453b6af54c7ad8eb0ae40a908949321757fd64cb
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-07-26 14:19:28 +0200

    redesign home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,14 +22,17 @@ export class HomeComponent implements OnInit, OnDestroy {
   starsText: string;
   siteTitle: string = undefined;
   siteDescriptions: string[];
+  siteDescription: string = '';
   // infoMessages: Message[] = [];
   // addSliderItem: SliderItem = undefined;
 
   followUs: SocialMedium[] = [
+    // {name: 'signal', shortcut: 'signal', urlBase: 'https://signal.group/#CjQKIHzeKjxDhPsOkmyVBuDyY4UfRo5aboltmuM6_DvKTmquEhD75Gre_8iJg4UB7zW6AhMY'},
+    {name: 'mastodon', shortcut: 'mastodon', urlBase: 'https://tldr.nettime.org/@screensavergallery'},
     {name: 'telegram', shortcut: 'tlgrm', urlBase: 'https://t.me/screensavergallery'},
-    {name: 'discord', shortcut: 'dc', urlBase: 'https://discord.gg/QJtRUYptRR'},
-    {name: 'rss', shortcut: 'rss', urlBase: 'https://screensaver.metazoa.org/feed/'},
-    {name: 'X', shortcut: 'x', urlBase: 'https://twitter.com/ScreenSaverG'},
+    // {name: 'discord', shortcut: 'dc', urlBase: 'https://discord.gg/QJtRUYptRR'},
+    {name: 'rss', shortcut: 'rss', urlBase: 'https://sleep.screensaver.gallery/feed/'},
+    // {name: 'X', shortcut: 'x', urlBase: 'https://twitter.com/ScreenSaverG'},
     {name: 'instagram', shortcut: 'i', urlBase: 'https://www.instagram.com/screensavergallery/'},
     {name: 'facebook', shortcut: 'fb', urlBase: 'https://www.facebook.com/ScreenSaverGallery'},
     {name: 'github', shortcut: 'git', urlBase: 'https://github.com/ScreenSaverGallery'}
@@ -60,7 +63,9 @@ export class HomeComponent implements OnInit, OnDestroy {
           this.siteTitle = info.name;
           this.siteDescriptions = info.description.split('; ');
           console.log('siteDescriptions', this.siteDescriptions);
-          
+          if (this.siteDescriptions.length) {
+            this.siteDescription = this.chooseRandomHeadline(this.siteDescriptions);
+          }
           setTimeout(() => {
             // console.log('waving elm', this.wavingElm);
             if (this.wavingElm) {
