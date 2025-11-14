e6d8ca18936303ac4d7202e8525d6ae484f5a308
tomaszatoo <xvjavurek@vutbr.cz>
2025-11-14 19:44:01 +0100

    we need to unify and centralise social contacts, so here is some first step..., enjoy! will see, if this will work in next modifications

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { SocialMedium } from 'src/app/models/social-medium';
+import { SocialMedium, followUs } from 'src/app/models/social-medium';
 // services
 import { ColorService } from 'src/app/services/color/color.service';
 import { NgStyle } from '@angular/common';
@@ -17,16 +17,6 @@ export class ContactComponent {
   private colorService = inject(ColorService);
   randomColor: string = this.colorService.generateHslaColors(100)[0];
 
-  followUs: SocialMedium[] = [
-    // {name: 'signal', shortcut: 'signal', urlBase: 'https://signal.group/#CjQKIHzeKjxDhPsOkmyVBuDyY4UfRo5aboltmuM6_DvKTmquEhD75Gre_8iJg4UB7zW6AhMY'},
-    {name: 'mastodon', shortcut: 'mastodon', urlBase: 'https://tldr.nettime.org/@screensavergallery'},
-    {name: 'telegram', shortcut: 'tlgrm', urlBase: 'https://t.me/screensavergallery'},
-    // {name: 'discord', shortcut: 'dc', urlBase: 'https://discord.gg/QJtRUYptRR'},
-    {name: 'rss', shortcut: 'rss', urlBase: 'https://sleep.screensaver.gallery/feed/'},
-    // {name: 'X', shortcut: 'x', urlBase: 'https://twitter.com/ScreenSaverG'},
-    {name: 'instagram', shortcut: 'i', urlBase: 'https://www.instagram.com/screensavergallery/'},
-    {name: 'facebook', shortcut: 'fb', urlBase: 'https://www.facebook.com/ScreenSaverGallery'},
-    {name: 'github', shortcut: 'git', urlBase: 'https://github.com/ScreenSaverGallery'}
-  ];
+  followUs: SocialMedium[] = followUs;
 
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -15,7 +15,7 @@ import { Menu, Post, MenuItem } from '@tomaszatoo/ngx-wp-api';
 // router
 import { Router, NavigationEnd, RouterLink, RouterLinkActive } from '@angular/router';
 // models
-import { SocialMedium } from '../../models/social-medium';
+import { SocialMedium, followUs } from '../../models/social-medium';
 // services
 import { WpService } from '../../services/wp/wp.service';
 import { ThemeService } from '../../services/theme/theme.service';
@@ -86,19 +86,7 @@ export class MenuComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private routerSub: Subscription = new Subscription();
 
-  followUs: SocialMedium[] = [
-    // {name: 'signal', shortcut: 'signal', urlBase: 'https://signal.group/#CjQKIHzeKjxDhPsOkmyVBuDyY4UfRo5aboltmuM6_DvKTmquEhD75Gre_8iJg4UB7zW6AhMY'},
-    {name: 'mastodon', shortcut: 'mastodon', urlBase: 'https://tldr.nettime.org/@screensavergallery'},
-    {name: 'telegram', shortcut: 'tlgrm', urlBase: 'https://t.me/screensavergallery'},
-    {name: 'matrix', shortcut: 'matrix', urlBase: 'https://matrix.to/#/#screensaver.gallery:matrix.org'},
-    // {name: 'discord', shortcut: 'dc', urlBase: 'https://discord.gg/QJtRUYptRR'},
-    {name: 'rss', shortcut: 'rss', urlBase: 'https://sleep.screensaver.gallery/feed/'},
-    // {name: 'X', shortcut: 'x', urlBase: 'https://twitter.com/ScreenSaverG'},
-    {name: 'instagram', shortcut: 'i', urlBase: 'https://www.instagram.com/screensavergallery/'},
-    {name: 'facebook', shortcut: 'fb', urlBase: 'https://www.facebook.com/ScreenSaverGallery'},
-    {name: 'github', shortcut: 'git', urlBase: 'https://github.com/ScreenSaverGallery'},
-    {name: 'newsletter', shortcut: 'monk', urlBase: 'https://listmonk.screensaver.gallery/subscription/form'}
-  ];
+  followUs: SocialMedium[] = followUs;
 
   private _openedMenu: boolean = false;
   private _space: number = 24;
