0c435496fc274bedb0469642c31e071658ea80b4
tomaszatoo <43030567+tomaszatoo@users.noreply.github.com>
2026-08-27 12:22:53 +0200

    Update social-medium.ts

--- src/app/models/social-medium.ts
+++ src/app/models/social-medium.ts
@@ -5,12 +5,12 @@ export interface SocialMedium {
 }
 
 export const followUs: SocialMedium[] = [
-    {name: 'mastodon', shortcut: 'mastodon', urlBase: 'https://tldr.nettime.org/@screensavergallery'},
+    {name: 'fediverse', shortcut: 'fedi', urlBase: 'https://tldr.nettime.org/@screensavergallery'},
     {name: 'telegram', shortcut: 'tlgrm', urlBase: 'https://t.me/screensavergallery'},
     {name: 'matrix', shortcut: 'matrix', urlBase: 'https://matrix.to/#/#screensaver.gallery:matrix.org'},
     {name: 'rss', shortcut: 'rss', urlBase: 'https://sleep.screensaver.gallery/feed/'},
     {name: 'instagram', shortcut: 'i', urlBase: 'https://www.instagram.com/screensavergallery/'},
-    {name: 'facebook', shortcut: 'fb', urlBase: 'https://www.facebook.com/ScreenSaverGallery'},
+    // {name: 'facebook', shortcut: 'fb', urlBase: 'https://www.facebook.com/ScreenSaverGallery'},
     {name: 'github', shortcut: 'git', urlBase: 'https://github.com/ScreenSaverGallery'},
     {name: 'newsletter', shortcut: 'monk', urlBase: 'https://listmonk.screensaver.gallery/subscription/form'}
 ];
